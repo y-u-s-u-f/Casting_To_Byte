16f29f9e433d184d3bd0ae6996c0183303ad1fee
YUSUF <YUSUF@DESKTOP-J1EOHH4>
2021-03-06 22:15:28 +0300

    Homework-1

---
+++ src/com/yusuf/toByte.java
@@ -0,0 +1,21 @@
+package com.yusuf;
+
+public class toByte {
+
+		public static void main(String[] args) {
+			
+			byte short_to_byte = (byte)Short.MAX_VALUE;
+			byte int_to_byte = (byte)Integer.MAX_VALUE;
+			byte long_to_byte = (byte)Long.MAX_VALUE;
+			byte float_to_byte = (byte)Float.MAX_VALUE;
+			byte double_to_byte = (byte)Double.MAX_VALUE;
+			
+            System.out.println("Short to Byte = " + short_to_byte );	
+            System.out.println("Int to Byte = " + int_to_byte );
+            System.out.println("Long to Byte = " + long_to_byte );
+            System.out.println("Float to Byte = " + float_to_byte );
+            System.out.println("Double to Byte = " + double_to_byte );
+		}
+
+
+}
